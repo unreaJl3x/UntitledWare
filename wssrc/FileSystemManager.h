307460f20680118886c875b08a791d099c0509d8
wssrc <107952844+unreaJl3x@users.noreply.github.com>
2024-11-15 11:38:37 +0300

    Update FileSystemManager.h

--- wssrc/FileSystemManager.h
+++ wssrc/FileSystemManager.h
@@ -2,16 +2,21 @@
 #define UNTITLEDWARE_FILESYSTEMMANAGER_H
 
 #include <fstream>
-#include "app.h"
+#include <iostream>
+#include "output.h"
+using namespace std;
 
-class FileSystemManager : public App {
+class FileSystemManager {
 private:
     char lastLetter = ';';
+Output* out;
+
 public:
     string folderInDirrectory="";
 
     bool CheckAvalaibleFile(string);
     bool WriteInFile(string, string);
+FileSystemManager(Output*);
 };
 
 
