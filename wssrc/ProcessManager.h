67ac898c7bda6974971825e41b0ed57d831bf5c9
wssrc <107952844+unreaJl3x@users.noreply.github.com>
2024-11-15 10:59:42 +0300

    Update ProcessManager.h

--- wssrc/ProcessManager.h
+++ wssrc/ProcessManager.h
@@ -1,11 +1,25 @@
 #ifndef UNTITLEDWARE_PROCESSMANAGER_H
 #define UNTITLEDWARE_PROCESSMANAGER_H
 
+#include <iostream>
+#include <windows.h>
+#include <Tlhelp32.h>
+
+#include "output.h"
 #include "app.h"
+#include "FileManager.h"
+using namespace std;
 
-class ProcessManager : public App {
+class ProcessManager {
+private:
+Output* out;
+App* victim;
 
+public:
+bool Write();
+template <typename T>
+T Read();
+ProcessManager(Output*,App*);
 };
-
 
 #endif //UNTITLEDWARE_PROCESSMANAGER_H
