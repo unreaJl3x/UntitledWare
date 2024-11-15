f31eb28cc3e00bc7a30ffa1972ed97a1423f349f
wssrc <107952844+unreaJl3x@users.noreply.github.com>
2024-11-15 11:47:18 +0300

    Update app.cpp

--- wssrc/app.cpp
+++ wssrc/app.cpp
@@ -4,7 +4,8 @@ App::App(string nameApp, string nameExe, string version) {
     this->nameApp = nameApp;
 this->nameExe = nameExe;
     this->version = version;
-this->pm = *new ProcessManager();
+this->pId=pm.GetPId();
+this->pHandle=pm.GetHandle();
 }
 
 void App::SetHeader(string text) {
