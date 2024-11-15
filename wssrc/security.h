186e49737e4b68eb1d64c82d6f4c847e47d44afb
wssrc <107952844+unreaJl3x@users.noreply.github.com>
2024-11-15 11:22:26 +0300

    Update security.h

--- wssrc/security.h
+++ wssrc/security.h
@@ -1,4 +1,21 @@
 #ifndef UNTITLEDWARE_SECURITY
 #define UNTITLEDWARE_SECURITY
 
+#include <iostream>
+
+#include "decomple.h"
+#include "output.h"
+
+using namespace std;
+
+class Security {
+private:
+Output* out;
+FileSystemManager*fm;
+bool CheckValid;
+
+public:
+void Start();
+Secure(Output*,FileSystemManager*);
+}
 #endef
